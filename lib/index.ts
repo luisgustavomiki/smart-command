1063f071b9455890bdbce3a0aad0e1b6aa8813d5
Luis Gustavo Miki <g.miki@live.com>
2018-06-11 09:27:17 +0000

    Adds events to scope and changes initialization.

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 import { ParameterList } from "./parameter_list";
 import { Command } from "./command";
+import { EventEmitter } from "events";
 
 
 /*
@@ -14,13 +15,28 @@ playerCommands.addCommnand('ban', {
 });
 */
 
-export class Scope {
+var scope_list: Scope[] = [];
+
+export class Scope extends EventEmitter {
   private commands: Command[] = [];
   /**
    * Creates a new scope and sets a provided name to it.
    * @param name The name of the scope to be used for logging and identification.
    */
-  constructor(public readonly name: string) {}
+  private constructor(public readonly name: string) {
+    super();
+  }
+
+  static get(name: string) {
+    var result = scope_list.find(s => s.name == name);
+    if(result) {
+      return result;
+    } else {
+      var scope = new Scope(name);
+      scope_list.push(scope);
+      return scope;
+    }
+  }
 
   public addCommand(name: string, parameters: any, handler: (source: any, ...args: any[]) => void) {
     var parameterListInstance = new ParameterList(parameters);
@@ -33,14 +49,14 @@ export class Scope {
     var command = this.commands.find(c => c.name == words[0]);
 
     if(!command) {
-      // handle error
+      this.emit('commandNotFound', input);
       return;
     }
 
     try {
       command.run(source, words.slice(1).join(' '));
     } catch(error) {
-      
+      this.emit('commandError', error, command.name, words.slice(1).join(' '));
     }
   }
 }
--- test/command.ts
+++ test/command.ts
@@ -1,12 +1,13 @@
 import { expect } from 'chai';
 import 'mocha';
+
 import { Command } from '../lib/command';
 import { ParameterList } from '../lib/parameter_list';
 import { Scope } from '../lib';
 
 describe('Command', () => {
   it('should process correctly a single Number', () => {
-    var scope = new Scope('command');
+    var scope = Scope.get('command');
     scope.addCommand('test', {number: 'Number'},  (source, parameters) => {
       expect(parameters.number).to.eq(10);
     });
@@ -14,7 +15,7 @@ describe('Command', () => {
   });
 
   it('should process correctly a Word followed by a Number', () => {
-    var scope = new Scope('command');
+    var scope = Scope.get('command');
     scope.addCommand('test', {name: 'Word', number: 'Number'},  (source, parameters) => {
       expect(parameters.number).to.eq(10);
       expect(parameters.word).to.eq('miki');
@@ -23,7 +24,7 @@ describe('Command', () => {
   });
 
   it('should process correctly a Word followed by a Number and then by a Phrase', () => {
-    var scope = new Scope('command');
+    var scope = Scope.get('command');
     scope.addCommand('test', {name: 'Word', number: 'Number', money: 'Phrase'},  (source, parameters) => {
       expect(parameters.number).to.eq(20);
       expect(parameters.word).to.eq('moko');
