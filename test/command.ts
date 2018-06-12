3bb1f5ac9a9da38e1832e4ce2d181ae3ce588740
Luis Gustavo Miki <g.miki@live.com>
2018-06-12 02:42:17 +0000

    Updates test for String TypeParser

--- test/command.ts
+++ test/command.ts
@@ -35,6 +35,17 @@ describe('Command', () => {
     scope.parse(null, `test     moko     20      "The quick brown \\"fox\\" jumps over the lazy dog."`);
   });
 
+  it('should process correctly a Word followed by a Number and then by a Phrase and finally by a String', () => {
+    var scope = Scope.get('command');
+    scope.addCommand('test', {name: 'Word', number: 'Number', money: 'Phrase', strang: 'String'},  (source, parameters) => {
+      expect(parameters.number).to.eq(20);
+      expect(parameters.word).to.eq('moko');
+      expect(parameters.money).to.eq('The quick brown "fox" jumps over the lazy dog.');
+      expect(parameters.strang).to.eq('teste teste teste teste');
+    });
+    scope.parse(null, `test     moko     20      "The quick brown \\"fox\\" jumps over the lazy dog." teste teste teste teste`);
+  });
+
   it('should emit an event with BlankParameterError when Number is blank', () => {
     var scope = Scope.get('command1');
     scope.addCommand('test', {number: 'Number'},  (source, parameters) => {
